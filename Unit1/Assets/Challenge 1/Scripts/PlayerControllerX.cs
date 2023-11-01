b8a425d8628178314af5ef97342ad193d3c89dd2
Dreglover <deandre.glover@s.capregboces.org>
2023-11-01 12:56:36 -0400

    drive
    Made a tank drive through obstacles

---
+++ Unit1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float verticalInput;
+    float magnitude = 25.5f;
+   
+
+    private void Start()
+    {
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Move the vehicle forward based on vertical input
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+
+    }
+}
